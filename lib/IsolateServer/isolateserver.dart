4e8df248942699b98b02ac7ba340681047894493
Kleak <segaud.kevin@gmail.com>
2014-02-03 17:38:54 +0100

    move class TempFile in file tempfile.dart

--- lib/IsolateServer/isolateserver.dart
+++ lib/IsolateServer/isolateserver.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'dart:convert';
 
 part 'src/request.dart';
+part 'src/tempfile.dart';
 part 'src/datamanager.dart';
 
 class IsolateServer {
--- lib/IsolateServer/src/datamanager.dart
+++ lib/IsolateServer/src/datamanager.dart
@@ -1,54 +1,5 @@
 part of IsolateServer;
 
-class TempFile {
-  String pathFile;
-  
-  TempFile(String dirPath) {
-    pathFile = dirPath + "tmp";
-  }
-  
-  void update(IO.HttpRequest req, IO.HttpServer hs, Request data) {
-    IO.File tmpFile = new IO.File(pathFile);
-    if (!tmpFile.existsSync())
-      tmpFile.createSync();
-    Map dataTmpFile = new Map<String, dynamic>();
-    Map session = new Map<dynamic, dynamic>();
-    req.session.forEach((dynamic key, dynamic value) {
-      session[key] = value;
-    });
-    dataTmpFile["SESSION"] = session;
-    
-    Map userInfo = new Map<dynamic, dynamic>();
-    userInfo["REMOTE_ADDRESS"] = req.connectionInfo.remoteAddress.address;
-    userInfo["REMOTE_PORT"] = req.connectionInfo.remotePort;
-    userInfo["LOCAL_PORT"] = req.connectionInfo.localPort;
-    dataTmpFile["USER_INFO"] = userInfo;
-    
-    Map requestInfo = new Map<String, dynamic>();
-    requestInfo["TYPE"] = data.type;
-    Map queryParameter = new Map<String, String>();
-    data.queryParam.forEach((key, value) {
-      queryParameter[key] = value;
-    });
-    requestInfo["QUERYPARAM"] = queryParameter; 
-    if (data.type == "POST") {
-      Map postData = new Map<String, String>();
-      String postSData = new String.fromCharCodes(data.data);
-      List<String> listData = postSData.split("=");
-      if (listData.length == 2) {
-        for (int i = 0; i < listData.length; i += 2)
-          postData[listData[i]] = Uri.decodeQueryComponent(listData[i + 1]);
-      }
-      else
-        postData["DEFAULT"] = postSData;
-      requestInfo["DATA"] = postData;
-    }
-    dataTmpFile["REQUEST"] = requestInfo;
-
-    tmpFile.writeAsStringSync(JSON.encode(dataTmpFile), mode: IO.WRITE);
-  }
-}
-
 class StaticFile {
   StaticFile(String dirPath) {
     
