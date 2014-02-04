d050519382626525cac1abaf93ac5803b6cc0102
Kleak <segaud.kevin@gmail.com>
2014-02-04 17:29:03 +0100

    delete request.dart

--- lib/IsolateServer/src/request.dart
+++
@@ -1,9 +0,0 @@
-part of IsolateServer;
-
-class Request {
-  String        type;
-  String        data;
-  Map           queryParam;
-  
-  Request(String this.type, String this.data, Map this.queryParam);
-}
